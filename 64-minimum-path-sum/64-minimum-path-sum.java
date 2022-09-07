feea3231ac07ca2903cde8b710b42dbaf80b831b
Vikas Singh <80041195+Vikas-Singh-10@users.noreply.github.com>
2022-09-07 11:39:31 +0530

    Time: 10 ms (5.96%), Space: 46.4 MB (23.31%) - LeetHub

---
+++ 64-minimum-path-sum/64-minimum-path-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if(grid==null || grid.length == 0 || grid[0].length == 0 ){
+            return 0;
+        }
+        int rows = grid.length;
+        int columns = grid[0].length;
+        int[][] dp = new int[rows][columns];
+        dp[0][0] = grid[0][0];
+        for(int i = 1; i < rows; i++){
+            dp[i][0] = dp[i-1][0] + grid[i][0];
+        }
+        for(int i = 1; i < columns; i++){
+            dp[0][i] = dp[0][i-1] + grid[0][i];
+        }
+        for(int i = 1; i< rows; i++){
+            for(int j =1; j < columns; j++){
+                dp[i][j] = Math.min(grid[i][j] + dp[i][j-1],grid[i][j]+dp[i-1][j] );
+            }
+        }
+        return dp[rows-1][columns-1];
+    }
+}
