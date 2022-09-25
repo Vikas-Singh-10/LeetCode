a06ff9ee543e53a17d43e7d121d146078c8cc675
Vikas Singh <80041195+Vikas-Singh-10@users.noreply.github.com>
2022-09-25 09:41:06 +0530

    Time: 17 ms (36.19%), Space: 44.5 MB (63.10%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,5 +1,8 @@
 class Solution {
     public boolean isPalindrome(int x) {
+        if(x < 0){
+            return false;
+        }
         int palin = 0;
         for(int i = x; i > 0; i/=10){
             int n = i % 10;
